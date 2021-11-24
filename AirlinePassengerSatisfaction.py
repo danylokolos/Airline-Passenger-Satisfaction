c1450b03696c70aa33552c9604097050f7400544
danylokolos <93236960+danylokolos@users.noreply.github.com>
2021-11-23 22:41:16 -0700

    Added results

--- AirlinePassengerSatisfaction.py
+++ AirlinePassengerSatisfaction.py
@@ -5,17 +5,43 @@ Created on Wed Nov 17 21:19:52 2021
 @author: Danylo
 """
 
+
+
+
 ### Read in Data
+import numpy as np
 import pandas as pd
 import pickle
+import os
+from collections import Counter
+
+
 
 infile = "Data_PassengerSatisfaction.csv"
 df = pd.read_csv(infile)
 
 
 
+
+### Exploratory Data Analysis
+from pandas_profiling import ProfileReport
+
+outfile = "PandasProfilingReport-AirlinePassengerSatisfaction.html"
+if os.path.exists(outfile) == False:
+    profile = ProfileReport(df)
+    profile.to_file(outfile)
+
+
+
+
 ### Data Wrangling
-# none needed,
+# get rid of missing values in "Arrival Delay in Minutes"
+df.shape
+df = df.dropna(axis=0, how="any")
+df.shape
+
+
+
 
 
 
@@ -55,6 +81,7 @@ lenc = LabelEncoder()
 _y = df[['satisfaction']].values
 lenc.fit(_y.ravel())
 df[['satisfaction']] = lenc.transform(_y.ravel())
+target_possibilities = df.satisfaction.unique()
 
 # save label encoder
 lenc_filename= "lenc_target.pkl"
@@ -63,16 +90,172 @@ with open(lenc_filename, 'wb') as outfile:
 
 
 
+
+
+
 ### Split data
 from sklearn.model_selection import train_test_split
 X = df[feature_names]
 y = df[target_names]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+random_state = 42
+test_size = 0.2
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 
 
-### ML
+# Undersample Majority Class - Random - results in lower accuracy
+"""
+from imblearn.under_sampling import RandomUnderSampler
+counter = Counter(y_train)
+print('Before',counter)
+undersample = RandomUnderSampler(sampling_strategy='majority')
+X_train_under, y_train_under = undersample.fit_resample(X_train, y_train)
+counter = Counter(y_train_under)
+print('After',counter)
+
+X_train = X_train_under
+y_train = y_train_under
+"""
 
 
 
-### Results
+### Machine Learning Model
+from sklearn.ensemble import RandomForestClassifier
+
+n_estimators = 100
+random_state = 42
+model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
+model.fit(X_train,y_train.values.ravel())
+
+
+# save model
+model_filename = "model_RF.pkl"
+with open(model_filename, 'wb') as outfile:
+    pickle.dump(model,outfile)
+    
+
+
+
+### Run Model on Test Dataset
+y_test_pred = model.predict(X_test)
+
+
+
+
+### Analyze Results
+# Feature Importance
+feature_importance = list(zip(X_train, model.feature_importances_))
+display('========== Feature Importance ==========')
+display(feature_importance)
+display('========== END ==========')
+
+# Accuracy Score
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test,y_test_pred)
+display('========== Accuracy Score ==========')
+display(accuracy)
+display('========== END ==========')
+
+
+# Confusion Matrix
+from sklearn.metrics import confusion_matrix
+conf_matrix = confusion_matrix(y_test, y_test_pred)
+
+display('========== Confusion Matrix ==========')
+display(conf_matrix)
+display('========== END ==========')
+
+# Plot Confusion Matrix
+import matplotlib.pyplot as plt
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Oranges):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Source: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #print("Normalized confusion matrix")
+    else:
+        _a = 1
+        #print('Confusion matrix, without normalization')
+
+    #print(cm)
+
+    # Plot the confusion matrix
+    plt.figure(figsize = (10, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, size = 24)
+    plt.colorbar(aspect=4)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=30, size = 14)
+    plt.yticks(tick_marks, classes, size = 14)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    
+    # Labeling the plot
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), fontsize = 20,
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+        
+    plt.grid(None)
+    plt.tight_layout()
+    plt.ylabel('True label', size = 18)
+    plt.xlabel('Predicted label', size = 18)
+
+# Confusion matrix
+plot_confusion_matrix(conf_matrix, classes = lenc.inverse_transform(target_possibilities),
+                      title = 'Customer Satisfaction Confusion Matrix')
+
+plt.savefig('RF_ConfusionMatrix.png')
+plt.show()
+
+
+
+# Plot Precision-Recall Curve
+from sklearn.metrics import PrecisionRecallDisplay
+
+#y_score = clf.decision_function(X_test)
+y_score = model.predict_proba(X_test)
+y_score = y_score[:,1]
+
+plt.figure()
+disp = PrecisionRecallDisplay.from_predictions(y_test, y_score, name="Random Forest")
+plt.title('Precision Recall Curve')
+plt.xlim([0,1.02])
+plt.ylim([0,1.02])
+plt.legend(loc = 'lower left')
+plt.savefig('RF_PrecisionRecallCurve.png')
+plt.show()
+
+
+# plot ROC Curve for single class
+import sklearn.metrics as metrics
+# calculate the fpr and tpr for all thresholds of the classification
+
+probs = model.predict_proba(X_test)
+preds = probs[:,1]
+fpr, tpr, threshold = metrics.roc_curve(y_test, preds)
+roc_auc = metrics.auc(fpr, tpr)
+
+# method I: plt
+import matplotlib.pyplot as plt
+plt.figure()
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([-0.02, 1])
+plt.ylim([0, 1.02])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.savefig('RF_ROCCurve.png')
+plt.show()
