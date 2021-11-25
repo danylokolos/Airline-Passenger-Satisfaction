d014196ca5bacbe42849192669fb44ff465b6b91
danylokolos <93236960+danylokolos@users.noreply.github.com>
2021-11-25 00:03:35 -0700

    small edits

--- AirlinePassengerSatisfaction.py
+++ AirlinePassengerSatisfaction.py
@@ -166,56 +166,29 @@ display('========== Confusion Matrix ==========')
 display(conf_matrix)
 display('========== END ==========')
 
+
+
+
 # Plot Confusion Matrix
 import matplotlib.pyplot as plt
-import itertools
-
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Oranges):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    Source: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        #print("Normalized confusion matrix")
-    else:
-        _a = 1
-        #print('Confusion matrix, without normalization')
-
-    #print(cm)
-
-    # Plot the confusion matrix
-    plt.figure(figsize = (10, 10))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title, size = 24)
-    plt.colorbar(aspect=4)
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=30, size = 14)
-    plt.yticks(tick_marks, classes, size = 14)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    
-    # Labeling the plot
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), fontsize = 20,
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-        
-    plt.grid(None)
-    plt.tight_layout()
-    plt.ylabel('True label', size = 18)
-    plt.xlabel('Predicted label', size = 18)
-
-# Confusion matrix
-plot_confusion_matrix(conf_matrix, classes = lenc.inverse_transform(target_possibilities),
-                      title = 'Customer Satisfaction Confusion Matrix')
-
+plt.figure(figsize = (10, 10))
+cmap = plt.cm.Blues
+plt.imshow(conf_matrix,cmap=cmap)
+plt.grid(None)
+plt.title('Customer Satisfaction Confusion Matrix', size = 24)
+plt.colorbar(aspect=5)
+output_labels = lenc.inverse_transform(target_possibilities)
+tick_marks = np.arange(len(output_labels))
+plt.xticks(tick_marks,output_labels,rotation=30)
+plt.yticks(tick_marks,output_labels)
+for ii in range(len(output_labels)):
+    for jj in range(len(output_labels)):
+        if conf_matrix[ii,jj] > np.max(conf_matrix)/2:
+            plt.text(ii,jj,conf_matrix[ii,jj],horizontalalignment="center",color="white")
+        else:
+            plt.text(ii,jj,conf_matrix[ii,jj],horizontalalignment="center")
 plt.savefig('RF_ConfusionMatrix.png')
+
 plt.show()
 
 
